99fc98e6b70cb5c581218b68072e9e7d985c89a0
Priyanka Pundir <priyanka.pundir09@gmail.com>
2022-01-23 14:56:49 +0000

    changes made from theia lab environment

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    var principal = document.getElementById("principal");
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal.value * years * rate /100;
+    var year = new Date().getFullYear()+parseInt(years);
+
+    // User enters zero or negative values, throw error.
+    if(principal.value <= 0){
+        alert("Enter a positive number.");
+        principal.focus();
+        return false;
+    }
+
+    //to display the result.
+    var result = ("If you deposit " + principal.value + "," + "\n" + "at an interest rate of " + rate + "%." + "\n" + "You will receive an amount of " + interest + "," + "\n" + "in the year " + year)
+    document.getElementById("result").innerText=result;
+
+    return true;
 }
-        
+
+function updateRate() 
+{
+    var rateval = (document.getElementById("rate").value + "%");
+    document.getElementById("rate_val").innerText=rateval;
+}
